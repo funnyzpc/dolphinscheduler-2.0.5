4c1c2a2cb102220b655fbad49126afe74f83816f
zpc <60003843@kans.cn>
2022-04-19 16:12:54 +0800

    init code 004

--- dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/datax.vue
+++ dolphinscheduler-ui/src/js/conf/home/pages/dag/_source/formModel/tasks/datax.vue
@@ -1,19 +1,3 @@
-/*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 <template>
   <div class="datax-model">
     <m-list-box>
@@ -100,7 +84,7 @@
           <span>{{$t('SpeedByte')}}</span>
         </div>
         <div slot="content">
-          <m-select-input v-model="jobSpeedByte" :list="[0,1,10,50,100,512]">
+          <m-select-input v-model="jobSpeedByte" :list="[0,1,10,50,100,512,1024,4096]">
           </m-select-input>
           <span>({{$t('0 means unlimited by byte')}})</span>
         </div>
@@ -110,7 +94,7 @@
           <span>{{$t('SpeedRecord')}}</span>
         </div>
         <div slot="content">
-          <m-select-input v-model="jobSpeedRecord" :list="[0,500,1000,1500,2000,2500,3000]">
+          <m-select-input v-model="jobSpeedRecord" :list="[0,500,1000,1500,2000,2500,3000,10000,20000]">
           </m-select-input>
           <span>({{$t('0 means unlimited by count')}})</span>
         </div>
